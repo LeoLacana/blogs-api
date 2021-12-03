7a600baea9453c1342b4b8f1b5a1a0b0e645743e
LeoLacana <wilson17414@gmail.com>
2021-12-03 20:26:35 -0300

    req 1

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-      username: 'root',
-      password: 'my-secret-pw',
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
       database: 'blogs_api',
-      host: '127.0.0.1',
+      host: process.env.HOSTNAME,
       dialect: 'mysql',
     },
     test: {
